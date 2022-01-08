5098cc94d93c2b3b36a6e2e5c95ebbfbb487853f
e <m18827526822@163.com>
2022-01-08 16:15:37 +0800

    lazyload

--- library_base/src/main/java/com/zjl/base/fragment/BaseFragment.kt
+++ library_base/src/main/java/com/zjl/base/fragment/BaseFragment.kt
@@ -1,10 +1,14 @@
 package com.zjl.base.fragment
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.viewbinding.ViewBinding
 import com.zjl.base.utils.autoCleared
 
@@ -19,7 +23,14 @@ import com.zjl.base.utils.autoCleared
  */
 abstract class BaseFragment<V: ViewBinding>: Fragment() {
 
+    lateinit var mActivity: AppCompatActivity
+
     protected var binding by autoCleared<V>()
+
+    private val handler = Handler()
+
+    //是否第一次加载
+    private var isFirst: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,4 +42,61 @@ abstract class BaseFragment<V: ViewBinding>: Fragment() {
     }
 
     abstract fun bindView(): V
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mActivity = context as AppCompatActivity
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        isFirst = true
+        initViewAndEvent()
+    }
+
+    /**
+     * 初始化视图和事件
+     * 用于初始化需要的View相关内容，包括其点击事件，初始状态等
+     */
+    abstract fun initViewAndEvent()
+
+    /**
+     * 懒加载
+     */
+    abstract fun lazyLoadData()
+
+
+    override fun onResume() {
+        super.onResume()
+        onVisible()
+    }
+
+    /**
+     * 是否需要懒加载
+     */
+    private fun onVisible() {
+        if (lifecycle.currentState == Lifecycle.State.STARTED && isFirst) {
+            // 延迟加载 防止 切换动画还没执行完毕时数据就已经加载好了，这时页面会有渲染卡顿
+            handler.postDelayed( {
+                lazyLoadData()
+                isFirst = false
+            },lazyLoadTime())
+        }
+    }
+
+    /**
+     * 延迟加载 防止 切换动画还没执行完毕时数据就已经加载好了，这时页面会有渲染卡顿  bug
+     * 这里传入你想要延迟的时间，延迟时间可以设置比转场动画时间长一点 单位： 毫秒
+     * 不传默认 300毫秒
+     * @return Long
+     */
+    open fun lazyLoadTime(): Long {
+        return 300
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
+    }
+
 }
