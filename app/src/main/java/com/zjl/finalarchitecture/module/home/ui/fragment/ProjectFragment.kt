f294067e4a425e2a513295f5c40fbc819a1e95e6
e <m18827526822@163.com>
2022-04-26 21:30:08 +0800

    【问题】paging3和下拉刷新上拉加载

--- app/src/main/java/com/zjl/finalarchitecture/module/home/ui/fragment/ProjectFragment.kt
+++ app/src/main/java/com/zjl/finalarchitecture/module/home/ui/fragment/ProjectFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.zjl.base.fragment.BaseFragment
+import com.zjl.base.utils.autoCleared
 import com.zjl.finalarchitecture.R
 import com.zjl.finalarchitecture.databinding.FragmentProjectBinding
+import com.zjl.finalarchitecture.module.home.ui.adapter.ArticleAdapter
+import com.zjl.finalarchitecture.module.home.viewmodel.AskViewModel
 
 /**
  * @description:首页项目
@@ -20,6 +24,11 @@ class ProjectFragment: BaseFragment<FragmentProjectBinding>() {
     companion object {
         fun newInstance() = ProjectFragment()
     }
+
+    private val mAskViewModel by viewModels<AskViewModel>()
+
+    private var mArticleAdapter by autoCleared<ArticleAdapter>()
+
 
     override fun bindView() = FragmentProjectBinding.inflate(layoutInflater)
 
