96350ba4570cf767c7130da2833f3ab0f1f16403
e <m18827526822@163.com>
2022-04-26 21:27:55 +0800

    【问题】paging3和下拉刷新上拉加载

--- app/src/main/java/com/zjl/finalarchitecture/module/home/ui/fragment/ProjectFragment.kt
+++ app/src/main/java/com/zjl/finalarchitecture/module/home/ui/fragment/ProjectFragment.kt
@@ -5,31 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.zjl.base.fragment.BaseFragment
 import com.zjl.finalarchitecture.R
+import com.zjl.finalarchitecture.databinding.FragmentProjectBinding
 
 /**
  * @description:首页项目
  * @author: zhou
  * @date : 2022/1/20 18:04
  */
-class ProjectFragment: Fragment() {
+class ProjectFragment: BaseFragment<FragmentProjectBinding>() {
 
 
     companion object {
         fun newInstance() = ProjectFragment()
     }
 
+    override fun bindView() = FragmentProjectBinding.inflate(layoutInflater)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun initViewAndEvent() {
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_project, container, false)
+    override fun createObserver() {
+
     }
 
 }
