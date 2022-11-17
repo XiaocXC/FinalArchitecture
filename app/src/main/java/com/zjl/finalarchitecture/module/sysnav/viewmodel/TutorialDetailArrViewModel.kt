c1beb4340d427ab61927bae2f4f4f038d75d39ed
周健力 <m18827526822@163.com>
2022-11-17 20:34:36 +0800

    send msg

---
+++ app/src/main/java/com/zjl/finalarchitecture/module/sysnav/viewmodel/TutorialDetailArrViewModel.kt
@@ -0,0 +1,13 @@
+package com.zjl.finalarchitecture.module.sysnav.viewmodel
+
+import com.zjl.base.viewmodel.BaseViewModel
+
+/**
+ * @description:
+ * @author: zhou
+ * @date : 2022/11/17 20:23
+ */
+class TutorialDetailArrViewModel : BaseViewModel() {
+
+
+}
