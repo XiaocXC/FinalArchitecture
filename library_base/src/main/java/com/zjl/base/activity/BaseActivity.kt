b9234804c57289ed867f0144e62c2f3139542cab
Xiaoc <1576999402@qq.com>
2023-01-13 16:07:50 +0800

    【完善】新增OriginActivity，它是所有Activity的根类，但是它不具备ViewModel和ViewBinding绑定的功能 【完善】新增OriginFragment，它是所有Fragment的根类，但是它不具备ViewModel和ViewBinding绑定的功能

--- library_base/src/main/java/com/zjl/base/activity/BaseActivity.kt
+++ library_base/src/main/java/com/zjl/base/activity/BaseActivity.kt
@@ -1,35 +1,11 @@
 package com.zjl.base.activity
 
-import android.os.Bundle
-import androidx.annotation.CallSuper
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
-import com.gyf.immersionbar.ImmersionBar
-import com.kongzue.dialogx.dialogs.WaitDialog
-import com.zjl.base.network.NetworkManager
-import com.zjl.base.globalContext
-import com.zjl.base.ui.onFailure
-import com.zjl.base.ui.onLoading
-import com.zjl.base.ui.onSuccess
-import com.zjl.base.ui.state.EmptyState
-import com.zjl.base.ui.state.ErrorState
-import com.zjl.base.ui.state.LoadingState
 import com.zjl.base.utils.ext.getVmClazz
 import com.zjl.base.utils.ext.inflateBindingWithGeneric
-import com.zjl.base.utils.ext.isNightMode
-import com.zjl.base.utils.launchAndCollectIn
 import com.zjl.base.viewmodel.BaseViewModel
-import com.zjl.lib_base.R
 import com.zjl.library_trace.base.IPageTrackNode
-import com.zjl.library_trace.base.ITrackNode
-import com.zjl.library_trace.base.TrackParams
-import com.zjl.library_trace.ext.getReferrerParams
-import com.zy.multistatepage.MultiStateContainer
-import com.zy.multistatepage.MultiStatePage.bindMultiState
-import com.zy.multistatepage.state.SuccessState
-import timber.log.Timber
 
 /**
  * @author Xiaoc
@@ -38,131 +14,17 @@ import timber.log.Timber
  * 基类Activity，提供基本的封装内容进行统一
  * 规定一个Activity要基于ViewBinding且有一个统一的ViewModel作为支撑
  */
-abstract class BaseActivity<V : ViewBinding, VM : BaseViewModel> : AppCompatActivity(), IPageTrackNode {
+abstract class BaseActivity<V : ViewBinding, VM : BaseViewModel> : OriginActivity(), IPageTrackNode {
 
     protected lateinit var mBinding: V
 
     protected lateinit var mViewModel: VM
 
-    /**
-     * 整个Activity的UiState状态控制器
-     */
-    protected val rootUiState by lazy {
-        bindMultiState(this)
-    }
-
-    // 来自上个页码的数据埋点快照
-    private var referrerSnapshot: ITrackNode? = null
-
-    // 这是一个默认的埋点数据值，你可以自定义Activity默认自带的埋点数据
-    protected open val defaultTrackParams by lazy {
-        TrackParams()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // 启动Activity后获取上个页码传来的埋点数据，如果存在，则设置快照数据
-        getReferrerSnapshot()?.let { referrerParams ->
-            referrerSnapshot = object: ITrackNode {
-                override val parentNode: ITrackNode?
-                    get() = null
-
-                override fun fillTrackParams(trackParams: TrackParams) {
-                    trackParams.merge(referrerParams)
-                }
-
-            }
-        }
-
-
+    override fun generateView() {
         mBinding = bindView()
         setContentView(mBinding.root)
 
-        // 设置沉浸式状态栏，此操作会去掉透明遮罩等内容
-        val defaultImmersionBar = ImmersionBar.with(this)
-            .transparentBar()
-            .statusBarDarkFont(!resources.isNightMode())
-            .navigationBarDarkIcon(!resources.isNightMode())
-
-        val immersionBar = configImmersive(defaultImmersionBar)
-        immersionBar?.init()
-
         mViewModel = bindViewModel()
-        initViewAndEvent(savedInstanceState)
-        createDefObserver()
-        createObserver()
-    }
-
-    // ---------------------------------------------------------------------------------------------
-    // 基本设置
-    // ---------------------------------------------------------------------------------------------
-
-    /**
-     * 配置沉浸式
-     * 默认Fragment不进行沉浸式处理，如果你需要请返回对应沉浸式处理的immersionBar
-     * @param immersionBar 默认的沉浸式处理，你可以自行创建，也可以在此基础上创建
-     */
-    open fun configImmersive(immersionBar: ImmersionBar): ImmersionBar?{
-        return immersionBar
-    }
-
-    /**
-     * 展示加载框弹窗（基于DialogX）
-     */
-    open fun showUiLoadingWithDialog(
-        message: String = getString(R.string.base_ui_description_status_view_loading),
-        cancelable: Boolean = false
-    ){
-        WaitDialog.show(message).apply {
-            isCancelable = cancelable
-        }
-    }
-
-    /**
-     * 展示加载界面
-     * @param message 加载提示
-     * @param uiState MultiStateContainer的视图对象，如果不传，默认就把整个Activity界面根视图变成加载界面
-     */
-    open fun showUiLoading(
-        message: String = getString(R.string.base_ui_description_status_view_loading),
-        uiState: MultiStateContainer = rootUiState)
-    {
-        uiState.show<LoadingState> {
-            it.setLoadingMsg(message)
-        }
-    }
-
-    /**
-     * 展示错误界面
-     * 提供一个默认实现，根据UiModel展示具体错误和重试逻辑
-     * @param throwable 错误信息
-     * @param uiState MultiStateContainer的视图对象，如果不传，默认就把整个Activity界面根视图变成失败界面
-     */
-    open fun showUiError(throwable: Throwable, uiState: MultiStateContainer = rootUiState) {
-        WaitDialog.dismiss()
-        uiState.show<ErrorState> {
-            it.setErrorMsg(throwable.message)
-            it.retry { retryAll() }
-        }
-    }
-
-    /**
-     * 展示成功页面
-     * 提供一个默认实现，及展示正确的视图，隐藏掉所有负面内容
-     * @param uiState MultiStateContainer的视图对象，如果不传，默认就把整个Activity界面根视图变成成功界面
-     */
-    open fun showUiSuccess(uiState: MultiStateContainer = rootUiState) {
-        WaitDialog.dismiss()
-        uiState.show(SuccessState())
-    }
-
-    /**
-     * 重试方法，默认不实现
-     * 子类可以重写该方法
-     */
-    open fun retryAll() {
-
     }
 
     /**
@@ -180,97 +42,6 @@ abstract class BaseActivity<V : ViewBinding, VM : BaseViewModel> : AppCompatActi
      */
     open fun bindViewModel(): VM {
         return ViewModelProvider(this)[getVmClazz(this)]
-    }
-
-    open fun createDefObserver() {
-        // 监听网络状态
-        // 我们规定监听网络状态的内容在Activity创建时开始，避免恢复Activity时重新观察的问题
-        NetworkManager.networkState.launchAndCollectIn(this, minActiveState = Lifecycle.State.CREATED){
-            val hasNetwork = NetworkManager.isConnectNetwork(globalContext)
-            Timber.i("Network：网络状态发生变化，是否有网络：%s", hasNetwork)
-            networkStateChanged(hasNetwork)
-        }
-    }
-
-    /**
-     * 当网络状态发生变化时回调，你可以重写此方法来进行逻辑操作
-     * @param hasNetwork 是否有网络
-     */
-    open fun networkStateChanged(hasNetwork: Boolean){}
-
-    /**
-     * 初始化视图和事件
-     * 用于初始化需要的View相关内容，包括其点击事件，初始状态等
-     */
-    abstract fun initViewAndEvent(savedInstanceState: Bundle?)
-
-    /**
-     * 创建观察者
-     */
-    abstract fun createObserver()
-
-    override fun onPause() {
-        super.onPause()
-        // 停止观察网络状态
-        NetworkManager.unregisterNetworkCallback(this)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        // 恢复观察网络状态
-        NetworkManager.registerNetworkCallback(this)
-    }
-
-    // ---------------------------------------------------------------------------------------------
-    // 埋点相关内容
-    // ---------------------------------------------------------------------------------------------
-
-    /**
-     * 获取上一个页面来的快照埋点数据
-     */
-    fun getReferrerSnapshot(): TrackParams? = intent.getReferrerParams()?.let { referrerParams ->
-        TrackParams().apply {
-            // 补充页面来的数据
-            fillReferrerKeyMap(referrerKeyMap(), referrerParams, this)
-        }
-    }
-
-    /**
-     * 通过映射值将上一个页面的值进行对应的更改
-     */
-    private fun fillReferrerKeyMap(
-        map: Map<String, String>?, referrerParams: TrackParams, params: TrackParams
-    ) {
-        if (map.isNullOrEmpty()) {
-            return
-        }
-        for ((fromKey, toKey) in map) {
-            val toValue = referrerParams[fromKey]
-            if (null != toValue) {
-                params.setIfNull(toKey, toValue)
-            }
-        }
-    }
-
-    /**
-     * 映射值Map
-     * @return map
-     * 跨页面后，上一个页面的埋点数据可能需要传递过来
-     * 例如 上一个页面 cur_page = "main" 传递来后
-     * 这个 cur_page Key可能需要变成 from_page
-     * 所以这里返回的Map就是对应的映射
-     */
-    override fun referrerKeyMap(): Map<String, String>? {
-        return null
-    }
-
-    override fun referrerSnapshot(): ITrackNode? {
-        return referrerSnapshot
-    }
-
-    @CallSuper
-    override fun fillTrackParams(trackParams: TrackParams) {
-        trackParams.merge(defaultTrackParams)
     }
 
 
--- library_base/src/main/java/com/zjl/base/fragment/BaseFragment.kt
+++ library_base/src/main/java/com/zjl/base/fragment/BaseFragment.kt
@@ -1,35 +1,14 @@
 package com.zjl.base.fragment
 
-import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.CallSuper
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
-import com.gyf.immersionbar.ImmersionBar
-import com.kongzue.dialogx.dialogs.WaitDialog
-import com.zjl.base.activity.BaseActivity
-import com.zjl.base.globalContext
-import com.zjl.base.network.NetworkManager
-import com.zjl.base.ui.state.ErrorState
-import com.zjl.base.ui.state.LoadingState
 import com.zjl.base.utils.ext.getVmClazz
 import com.zjl.base.utils.ext.inflateBindingWithGeneric
-import com.zjl.base.utils.ext.isNightMode
-import com.zjl.base.utils.launchAndCollectIn
 import com.zjl.base.viewmodel.BaseViewModel
-import com.zjl.lib_base.R
-import com.zjl.library_trace.base.ITrackModel
-import com.zjl.library_trace.base.TrackParams
-import com.zjl.library_trace.ext.trackModel
-import com.zy.multistatepage.MultiStateContainer
-import com.zy.multistatepage.bindMultiState
-import com.zy.multistatepage.state.SuccessState
-import timber.log.Timber
 
 /**
  * @author Xiaoc
@@ -40,57 +19,20 @@ import timber.log.Timber
  * 根据MVI或MVVM规定，一个界面由一个ViewModel所绑定，我们这里需要一个VM的绑定
  * 如果该界面需要用到多个VM，则请将负责界面状态的作为主要的VM，或者自行实现状态管理
  */
-abstract class BaseFragment<V : ViewBinding, VM : BaseViewModel> : Fragment() {
-
-    /**
-     * 当前Fragment是否可见
-     */
-    protected var isUserHintVisible: Boolean = false
+abstract class BaseFragment<V : ViewBinding, VM : BaseViewModel> : OriginFragment() {
 
     private var _mBinding: V? = null
     protected val mBinding get() = _mBinding!!
 
     protected lateinit var mViewModel: VM
 
-    /**
-     * 整个Fragment的状态控制器
-     */
-    protected lateinit var uiRootState: MultiStateContainer
-
-
-    @CallSuper
-    override fun onCreateView(
+    override fun generateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _mBinding = bindView(inflater, container, savedInstanceState)
-        uiRootState = mBinding.root.bindMultiState()
-        return uiRootState
-    }
-
-    private fun configImmersiveInternal(){
-        if(isUserHintVisible){
-            val activity = requireActivity()
-            if(activity is BaseActivity<*,*>){
-                // 设置沉浸式状态栏，此操作会去掉透明遮罩等内容
-                val defaultImmersionBar = ImmersionBar.with(this)
-                    .transparentBar()
-                    .statusBarDarkFont(!resources.isNightMode())
-                    .navigationBarDarkIcon(!resources.isNightMode())
-                val immersionBar = configImmersive(defaultImmersionBar)
-                immersionBar?.init()
-            }
-        }
-    }
-
-    /**
-     * 配置沉浸式
-     * Fragment进行默认沉浸式处理
-     * @param immersionBar 默认的沉浸式处理，你可以自行创建，也可以在此基础上创建
-     */
-    open fun configImmersive(immersionBar: ImmersionBar): ImmersionBar?{
-        return immersionBar
+        return mBinding.root
     }
 
     /**
@@ -106,99 +48,9 @@ abstract class BaseFragment<V : ViewBinding, VM : BaseViewModel> : Fragment() {
         return inflateBindingWithGeneric(inflater, container, false)
     }
 
-    @CallSuper
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        // 给Fragment的根View设置埋点
-        view.trackModel = ITrackModel {
-            fillTrackParams(it)
-        }
-
-        configImmersiveInternal()
-
+    override fun bindOthers() {
         mViewModel = createViewModel()
-
-        initViewAndEvent(savedInstanceState)
-        createDefObserver()
-        createObserver()
     }
-
-    /**
-     * 展示加载框弹窗（基于DialogX）
-     */
-    open fun showUiLoadingWithDialog(
-        message: String = getString(R.string.base_ui_description_status_view_loading),
-        cancelable: Boolean = false
-    ){
-        WaitDialog.show(message).apply {
-            isCancelable = cancelable
-        }
-    }
-
-    /**
-     * 展示加载界面
-     * @param message 加载提示
-     * @param uiState MultiStateContainer的视图对象，如果不传，默认就把整个Fragment界面根视图变成加载界面
-     */
-    open fun showUiLoading(
-        message: String = getString(R.string.base_ui_description_status_view_loading),
-        uiState: MultiStateContainer = uiRootState
-    ){
-        uiState.show<LoadingState> {
-            it.setLoadingMsg(message)
-        }
-    }
-
-    /**
-     * 展示错误界面
-     * 提供一个默认实现，根据UiModel展示具体错误和重试逻辑
-     * @param throwable 错误信息
-     * @param uiState MultiStateContainer的视图对象，如果不传，默认就把整个Fragment界面根视图变成失败界面
-     */
-    open fun showUiError(throwable: Throwable, uiState: MultiStateContainer = uiRootState) {
-        WaitDialog.dismiss()
-        uiState.show<ErrorState> {
-            it.setErrorMsg(throwable.message)
-            it.retry { retryAll() }
-        }
-    }
-
-    /**
-     * 展示成功页面
-     * 提供一个默认实现，及展示正确的视图，隐藏掉所有负面内容
-     * @param uiState MultiStateContainer的视图对象，如果不传，默认就把整个Fragment界面根视图变成成功界面
-     */
-    open fun showUiSuccess(uiState: MultiStateContainer = uiRootState) {
-        WaitDialog.dismiss()
-        uiState.show(SuccessState())
-    }
-
-    /**
-     * 重试刷新全部内容的方法
-     * 子类可以重写该方法
-     */
-    open fun retryAll() {
-
-    }
-
-    /**
-     * 创建默认的监听器内容
-     * 主要用于界面状态的监听
-     */
-    open fun createDefObserver() {
-        // 网络状态监听
-        NetworkManager.networkState.launchAndCollectIn(viewLifecycleOwner, minActiveState = Lifecycle.State.CREATED){
-            val hasNetwork = NetworkManager.isConnectNetwork(globalContext)
-            Timber.i("Network：网络状态发生变化，是否有网络：%s", hasNetwork)
-            networkStateChanged(hasNetwork)
-        }
-    }
-
-    /**
-     * 当网络状态发生变化时回调，你可以重写此方法来进行逻辑操作
-     * @param hasNetwork 是否有网络
-     */
-    open fun networkStateChanged(hasNetwork: Boolean){}
 
     /**
      * 利用反射创建ViewModel实例
@@ -206,55 +58,6 @@ abstract class BaseFragment<V : ViewBinding, VM : BaseViewModel> : Fragment() {
      */
     open fun createViewModel(): VM {
         return ViewModelProvider(this)[getVmClazz(this)]
-    }
-
-    /**
-     * 初始化视图和事件
-     * 用于初始化需要的View相关内容，包括其点击事件，初始状态等
-     */
-    abstract fun initViewAndEvent(savedInstanceState: Bundle?)
-
-    /**
-     * 创建观察者
-     */
-    abstract fun createObserver()
-
-    override fun onResume() {
-        super.onResume()
-        isUserHintVisible = true
-        configImmersiveInternal()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        isUserHintVisible = false
-        configImmersiveInternal()
-    }
-
-    override fun onHiddenChanged(hidden: Boolean) {
-        super.onHiddenChanged(hidden)
-        configImmersiveInternal()
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        configImmersiveInternal()
-    }
-
-    /**
-     * 当Fragment视图销毁时使用
-     * 如果Fragment需要手动释放部分视图内容时，我们使用请确保在调用 super.onDestroyView() 之前清除视图
-     */
-    override fun onDestroyView() {
-        super.onDestroyView()
-        // 保持ViewBinding在 onCreateView 和 onDestroyView 生命周期之间
-        // 这里不自动使用autoCleared原因是因为有个先后顺序，因为部分adapter的原因，需要重写onDestroyView来手动设置为Null
-        // 而此时如果获取mBinding可能由于清除了mBinding导致报错，所以我们会手动处理
-        _mBinding = null
-    }
-
-    protected open fun fillTrackParams(trackParams: TrackParams) {
-        // 默认不实现任何埋点数据填充
     }
 
 }
